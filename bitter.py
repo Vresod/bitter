ce3857d6e6734b733b2e3fd4022f1f0a3051361e
Vresod <lukebinkofsky@gmail.com>
2021-04-08 21:47:38 -0400

    add account system don't worry you can still be anon

--- bitter.py
+++ bitter.py
@@ -1,11 +1,13 @@
-from flask import Flask, request
-from flask import json
-from flask.json import jsonify
-from flask import render_template
-app = Flask(__name__)
+from flask import Flask, request, jsonify, render_template
 import json
 
+from flask.wrappers import Request
+import extra
+import hashlib
+app = Flask(__name__)
+
 posts = []
+accounts = [extra.Account("anon","",0)]
 
 """
 DESIGN:
@@ -17,9 +19,14 @@ def get_post_posts():
 	if request.method == 'GET': # return list of bleet
 		return jsonify(posts)
 	else: # make new bleet
-		posts.append({"content":request.form['content']})
-		return jsonify(posts)
-	
+		form = dict(request.form)
+		if 'id' not in form:
+			form['id'] = 0
+		elif hashlib.sha256(form.get('password').encode()).hexdigest() != extra.get_account(accounts,int(form['id'])).to_dict(internal=True)['passhash']:
+			return {"message":"403 Forbidden","code":403}, 403
+		posts.append({"content":form['content'],"author_id":form['id']})
+		return jsonify(posts), 201
+
 @app.route('/',methods=['GET'])
 def index():
 	output = ""
@@ -28,7 +35,23 @@ def index():
 	print(output)
 	return output
 
+@app.route('/accounts',methods=['GET','POST'])
+def get_post_accounts():
+	if request.method == 'GET': # DO NOT RETURN PASSHASH
+		accounts_no_passhash = []
+		for account in accounts:
+			accounts_no_passhash.append(account.to_dict())
+		return jsonify(accounts_no_passhash)
+	else: # making an account
+		form = dict(request.form)
+		account = extra.Account.from_dict(form,len(accounts))
+		accounts.append(account)
+		return account.to_dict(), 201
+
+@app.route('/accounts/<int:id>')
+def get_account(id):
+	return extra.get_account(accounts,int(id)).to_dict()
 
 @app.errorhandler(404)
 def http404(e):
-	return {"message":"404 Not Found","code":404}
+	return {"message":"404 Not Found","code":404}, 404
